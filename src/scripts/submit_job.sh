bfb786a5feed67052ff7601ff3e3e5f111418b18
Dream High <smartcoder0415@gmail.com>
2022-09-06 21:03:20 -0400

    updated cli

--- src/scripts/submit_job.sh
+++ src/scripts/submit_job.sh
@@ -1,1 +1,1 @@
-pipenv run engi submission execute ../same-story-api/test/data figma engi-network/same-story-storybook --env staging
+pipenv run engi submission execute ../same-story-api/test/data figma engi-network/same-story-storybook --knob label=Arse --knob primary=false --env staging
