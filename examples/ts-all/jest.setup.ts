7ec85f6f941388c753bece4790bc89db7ddc00e5
Dream High <smartcoder0415@gmail.com>
2022-09-09 01:43:53 -0400

    fixed jest setup

--- examples/ts-all/jest.setup.ts
+++ examples/ts-all/jest.setup.ts
@@ -1,6 +1,3 @@
 import { toBeSameStory } from "../../src/jest_plugin";
-// import "../../src/jest_plugin/all";
 
-console.log("=========>");
-// or just add specific matchers
 expect.extend({ toBeSameStory });
