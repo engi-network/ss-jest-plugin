68e925bcaa8e7013eb59b8ecff6f5cf124dd492d
Dream High <smartcoder0415@gmail.com>
2022-09-26 07:39:45 -0400

    testing jest parallel execution

--- examples/ts-all/test/same_story.test.ts
+++ examples/ts-all/test/same_story.test.ts
@@ -3,11 +3,11 @@ import { mockInputData, contactUsData } from "../src/data";
 const timeout = 60_000 * 3;
 
 describe("Expect same story", () => {
-  test("Are they the same story?", async () => {
+  test.concurrent("Are they the same story?", async () => {
     await expect(mockInputData).toBeSameStory();
   }, timeout);
 
-  test("Is Contact page the same story?", async () => {
+  test.concurrent("Is Contact page the same story?", async () => {
     await expect(contactUsData).toBeSameStory();
   }, timeout);
 
